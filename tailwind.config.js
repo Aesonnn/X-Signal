85fd648619a3a9cf07bf1c65e48bb9f03cbe939c
max <alexannilss@gmail.com>
2026-05-25 18:43:03 +0300

    Tailwind SDK

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./dashboard/templates/**/*.html",
+    "./dashboard/**/*.py",
+    "./templates/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
